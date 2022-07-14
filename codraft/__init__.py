40231f4cc247e6e782cdfc96a384373f8a59bef2
Pierre Raybaut <p.raybaut@codra.fr>
2022-07-14 16:28:31 +0200

    version

--- codraft/__init__.py
+++ codraft/__init__.py
@@ -17,7 +17,7 @@ libraries (such as NumPy, SciPy or OpenCV) and Qt graphical user interfaces (tha
 
 import os
 
-__version__ = "2.0.4"
+__version__ = "2.1.0"
 __docurl__ = "https://codraft.readthedocs.io/en/latest/"
 __homeurl__ = "https://codra-ingenierie-informatique.github.io/CodraFT/"
 __supporturl__ = (
