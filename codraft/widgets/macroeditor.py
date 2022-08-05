4302aa0edb70cf53fca6876f43b48b7455fb5a8b
Pierre Raybaut <p.raybaut@codra.fr>
2022-08-05 18:41:32 +0200

    Macro editor: added menu with actions

--- codraft/widgets/macroeditor.py
+++ codraft/widgets/macroeditor.py
@@ -10,7 +10,7 @@ Module providing CodraFT Macro editor widget
 import os.path as osp
 
 from guidata.configtools import get_icon
-from guidata.qthelpers import create_toolbutton
+from guidata.qthelpers import add_actions, create_action, create_toolbutton
 from guidata.widgets.codeeditor import CodeEditor
 from qtpy import QtCore as QC
 from qtpy import QtWidgets as QW
@@ -32,11 +32,10 @@ class Macro(QC.QObject):
         self.editor = CodeEditor(language="python")
         self.editor.set_text_from_file(self.MACRO_SAMPLE_PATH)
         self.objectNameChanged.connect(self.name_changed)
-        self.tabwidget.addTab(self.editor, self.objectName())
 
     def name_changed(self, name):
         """Macro name has been changed"""
-        index = self.tabwidget.indexOf(self)
+        index = self.tabwidget.indexOf(self.editor)
         self.tabwidget.setTabText(index, name)
 
     def get_untitled_title(self):
@@ -55,32 +54,130 @@ class MacroEditorWidget(QW.QTabWidget):
         self.setWindowIcon(get_icon("libre-gui-cogs.svg"))
         self.setTabsClosable(True)
         self.setMovable(True)
+        self.tabBarDoubleClicked.connect(self.rename_macro)
         self.tabCloseRequested.connect(self.remove_macro)
-        # TODO: Add a menu instead of the "add_button" (icon: libre-gui-menu.svg)
-        # TODO: Add action "Run" in menu
+
+        self.__macros = {}
+
         # TODO: Add action "Import from file..."
         # TODO: Add action "Export to file..."
-        # TODO: Add action "Remove"
 
+        self.setup_actions()
+
+    def setup_actions(self):
+        """Setup macro menu actions"""
+        run_act = create_action(
+            self,
+            _("Run macro"),
+            icon=get_icon("libre-camera-flash-on.svg"),
+            triggered=self.run_macro,
+        )
+        stp_act = create_action(
+            self,
+            _("Stop macro"),
+            icon=get_icon("libre-camera-flash-off.svg"),
+            triggered=self.stop_macro,
+        )
+        stp_act.setDisabled(True)
+        add_act = create_action(
+            self,
+            _("New macro"),
+            icon=get_icon("libre-gui-add.svg"),
+            triggered=self.add_macro,
+        )
+        ren_act = create_action(
+            self,
+            _("Rename macro"),
+            icon=get_icon("libre-gui-pencil.svg"),
+            triggered=self.rename_macro,
+        )
+        exp_act = create_action(
+            self,
+            _("Export macro to file"),
+            icon=get_icon("libre-gui-export.svg"),
+            triggered=self.export_macro_to_file,
+        )
+        imp_act = create_action(
+            self,
+            _("Import macro from file"),
+            icon=get_icon("libre-gui-import.svg"),
+            triggered=self.import_macro_from_file,
+        )
+        rem_act = create_action(
+            self,
+            _("Remove macro"),
+            icon=get_icon("libre-gui-action-delete.svg"),
+            triggered=self.remove_macro,
+        )
+        actions = (
+            run_act,
+            stp_act,
+            None,
+            add_act,
+            ren_act,
+            exp_act,
+            imp_act,
+            None,
+            rem_act,
+        )
+        menu_button = QW.QPushButton(get_icon("libre-gui-menu.svg"), "", self)
+        menu_button.setFlat(True)
+        menu = QW.QMenu()
+        menu_button.setMenu(menu)
+        add_actions(menu, actions)
+        self.setCornerWidget(menu_button)
+        self.add_macro(rename=False)
+
+    def run_macro(self):
+        """Run current macro"""
         # XXX: Macros should be executed in a separate process: access to CodraFT
         # is provided by the 'remote_controlling' feature (see corresponding branch).
         # So, macros should be Python scripts similar to "remoteclient_test.py".
         # Connection to the XML-RPC server should be simplified (to a single line).
 
-        add_button = create_toolbutton(
-            self,
-            icon=get_icon("libre-gui-add.svg"),
-            triggered=self.add_macro,
-        )
-        self.setCornerWidget(add_button)
-        self.add_macro()
+    def stop_macro(self):
+        """Stop current macro"""
 
-    def add_macro(self, name: str = None):
+    def add_macro(self, name: str = None, rename: bool = True):
         """Add macro, optionally with name"""
         macro = Macro(self, name)
+        index = self.addTab(macro.editor, macro.objectName())
+        self.__macros[id(macro)] = macro
+        if rename:
+            self.rename_macro(index)
 
-    def remove_macro(self, index: int):
+    def get_macro(self, index: int):
+        """Return macro at index"""
+        for macro in self.__macros.values():
+            if self.widget(index) is macro.editor:
+                return macro
+
+    def rename_macro(self, index: int = None):
+        """Rename macro"""
+        if index is None:
+            index = self.currentIndex()
+        macro = self.get_macro(index)
+        name, valid = QW.QInputDialog.getText(
+            self,
+            _("Rename"),
+            _("New title:"),
+            QW.QLineEdit.Normal,
+            macro.objectName(),
+        )
+        if valid:
+            macro.setObjectName(name)
+            self.setCurrentIndex(index)
+
+    def export_macro_to_file(self):
+        """Export macro to file"""
+
+    def import_macro_from_file(self):
+        """Import macro from file"""
+
+    def remove_macro(self, index: int = None):
         """Remove macro"""
+        if index is None:
+            index = self.currentIndex()
         txt = "<br>".join(
             [
                 _(
