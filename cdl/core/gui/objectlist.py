9508ab814f495bc4473809c3a7908d6af5e4c71b
Pierre Raybaut <p.raybaut@codra.fr>
2023-04-15 09:07:32 +0200

    Added some type annotations

--- cdl/core/gui/objectlist.py
+++ cdl/core/gui/objectlist.py
@@ -9,8 +9,10 @@ Object (signal/image) list widgets
 
 # pylint: disable=invalid-name  # Allows short reference names like x, y, ...
 
+from __future__ import annotations  # To be removed when dropping Python <=3.9 support
+
 import re
-from typing import Tuple
+from typing import TYPE_CHECKING, Iterator, List, Optional, Tuple
 
 from qtpy import QtCore as QC
 from qtpy import QtGui as QG
@@ -19,6 +21,10 @@ from qtpy import QtWidgets as QW
 from cdl.config import _
 from cdl.utils.qthelpers import block_signals
 
+if TYPE_CHECKING:
+    from cdl.core.gui.panel.base import BaseDataPanel
+    from cdl.core.model.base import ObjectItf
+
 
 class SimpleObjectList(QW.QListWidget):
     """Base object handling panel list widget, object (sig/ima) lists"""
@@ -26,30 +32,32 @@ class SimpleObjectList(QW.QListWidget):
     SIG_ITEM_DOUBLECLICKED = QC.Signal(int)
     SIG_CONTEXT_MENU = QC.Signal(QC.QPoint)
 
-    def __init__(self, panel, parent=None):
+    def __init__(self, panel: BaseDataPanel, parent: QW.QWidget = None) -> None:
         parent = panel if parent is None else parent
         super().__init__(parent)
         self.panel = panel
         self.prefix = panel.PREFIX
         self.setAlternatingRowColors(True)
-        self._objects = []  # signals or images
+        self._objects: List[ObjectItf] = []  # signals or images
         self.itemDoubleClicked.connect(self.item_double_clicked)
 
-    def init_from(self, objlist):
+    def init_from(self, objlist: SimpleObjectList) -> None:
         """Init from another SimpleObjectList, without making copies of objects"""
         self._objects = objlist.get_objects()
         self.refresh_list()
         self.setCurrentRow(objlist.currentRow())
 
-    def get_objects(self):
+    def get_objects(self) -> List[ObjectItf]:
         """Get all objects"""
         return self._objects
 
-    def get_titles(self):
+    def get_titles(self) -> List[str]:
         """Get object titles as diplayed in QListWidget"""
         return [self.item(row).text() for row in range(self.count())]
 
-    def set_current_row(self, row, extend=False, refresh=True):
+    def set_current_row(
+        self, row: int, extend: bool = False, refresh: bool = True
+    ) -> None:
         """Set list widget current row"""
         if row < 0:
             row += self.count()
@@ -60,7 +68,7 @@ class SimpleObjectList(QW.QListWidget):
         with block_signals(widget=self, enable=not refresh):
             self.setCurrentRow(row, command)
 
-    def refresh_list(self, new_current_row=None):
+    def refresh_list(self, new_current_row: Optional[int] = None) -> None:
         """
         Refresh object list
 
@@ -89,7 +97,7 @@ class SimpleObjectList(QW.QListWidget):
 class GetObjectDialog(QW.QDialog):
     """Get object dialog box"""
 
-    def __init__(self, parent, panel, title):
+    def __init__(self, parent: QW.QWidget, panel: BaseDataPanel, title: str) -> None:
         super().__init__(parent)
         self.setWindowTitle(title)
         self.setLayout(QW.QVBoxLayout())
@@ -106,7 +114,7 @@ class GetObjectDialog(QW.QDialog):
         self.layout().addSpacing(10)
         self.layout().addWidget(bbox)
 
-    def get_object(self):
+    def get_object(self) -> ObjectItf:
         """Return current object"""
         return self.objlist.get_objects()[self.objlist.currentRow()]
 
@@ -116,7 +124,7 @@ class ObjectList(SimpleObjectList):
 
     SIG_IMPORT_FILES = QC.Signal(list)
 
-    def __init__(self, panel):
+    def __init__(self, panel: BaseDataPanel) -> None:
         super().__init__(panel)
         self.setSelectionMode(QW.QListWidget.ExtendedSelection)
         self.setAcceptDrops(True)
@@ -154,23 +162,23 @@ class ObjectList(SimpleObjectList):
         else:
             event.ignore()
 
-    def __len__(self):
+    def __len__(self) -> int:
         """Return number of objects"""
         return len(self._objects)
 
-    def __getitem__(self, row):
+    def __getitem__(self, row: int) -> ObjectItf:
         """Return object at row"""
         return self._objects[row]
 
-    def __setitem__(self, row, obj):
+    def __setitem__(self, row: int, obj: ObjectItf) -> None:
         """Set object at row"""
         self._objects[row] = obj
 
-    def __contains__(self, obj):
+    def __contains__(self, obj: ObjectItf) -> bool:
         """Return True if list contain obj"""
         return obj in self._objects
 
-    def get_row(self, obj):
+    def get_row(self, obj: ObjectItf) -> int:
         """Return row associated to object obj"""
         return self._objects.index(obj)
 
@@ -191,16 +199,16 @@ class ObjectList(SimpleObjectList):
                     continue
                 obj.title = obj.title.replace(before, after)
 
-    def __delitem__(self, row):
+    def __delitem__(self, row: int) -> None:
         """Del object at row"""
         self.__fix_obj_titles(row, -1)
         self._objects.pop(row)
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[ObjectItf]:
         """Return an iterator over objects"""
         yield from self._objects
 
-    def get_sel_object(self, position=0):
+    def get_sel_object(self, position: int = 0) -> ObjectItf:
         """
         Return currently selected object
 
@@ -212,32 +220,32 @@ class ObjectList(SimpleObjectList):
             return self[rows[position]]
         return None
 
-    def get_sel_objects(self):
+    def get_sel_objects(self) -> List[ObjectItf]:
         """Return selected objects"""
         return [self[row] for row in self.get_selected_rows()]
 
-    def append(self, obj):
+    def append(self, obj: ObjectItf) -> None:
         """Append object"""
         self._objects.append(obj)
 
-    def insert(self, row, obj):
+    def insert(self, row: int, obj: ObjectItf) -> None:
         """Insert object at row index"""
         self.__fix_obj_titles(row, 1)
         self._objects.insert(row, obj)
 
-    def remove_all(self):
+    def remove_all(self) -> None:
         """Remove all objects"""
         self._objects = []
 
-    def select_rows(self, rows: Tuple):
+    def select_rows(self, rows: Tuple) -> None:
         """Select multiple list widget rows"""
         for index, row in enumerate(sorted(rows)):
             self.set_current_row(row, extend=index != 0, refresh=row == len(rows) - 1)
 
-    def select_all_rows(self):
+    def select_all_rows(self) -> None:
         """Select all widget rows"""
         self.selectAll()
 
-    def get_selected_rows(self):
+    def get_selected_rows(self) -> List[int]:
         """Return selected rows"""
         return [index.row() for index in self.selectionModel().selectedRows()]
