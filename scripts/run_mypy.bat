b50e0aff0aa15ba4d3864bcaabea490f81ed7d51
Pierre Raybaut <p.raybaut@codra.fr>
2023-05-11 12:38:58 +0200

    Added mypy analysis (task/script)

---
+++ scripts/run_mypy.bat
@@ -0,0 +1,17 @@
+@echo off
+REM This script was derived from PythonQwt project
+REM ======================================================
+REM Run mypy code analysis tool
+REM ======================================================
+REM Licensed under the terms of the MIT License
+REM Copyright (c) 2020 Pierre Raybaut
+REM (see PythonQwt LICENSE file for more details)
+REM ======================================================
+setlocal
+call %~dp0utils GetScriptPath SCRIPTPATH
+call %FUNC% GetLibName LIBNAME
+call %FUNC% GetModName MODNAME
+call %FUNC% SetPythonPath
+call %FUNC% UseWinPython
+mypy --strict --ignore-missing-imports %MODNAME%
+call %FUNC% EndOfScript
