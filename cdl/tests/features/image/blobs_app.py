5bebdbd40e249328746785467f09935111758015
Pierre Raybaut <p.raybaut@codra.fr>
2023-06-02 15:42:28 +0200

    Tests: added blob detection / edges app tests

---
+++ cdl/tests/features/image/blobs_app.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the terms of the BSD 3-Clause
+# (see cdl/LICENSE for details)
+
+"""
+Blob detection application test
+"""
+
+# pylint: disable=invalid-name  # Allows short reference names like x, y, ...
+
+from skimage.data import human_mitosis  # pylint: disable=no-name-in-module
+
+import cdl.param
+from cdl.core.model.image import create_image
+from cdl.tests import cdl_app_context
+
+SHOW = True  # Show test in GUI-based test launcher
+
+
+def test():
+    """Run ROI unit test scenario"""
+    with cdl_app_context() as win:
+        panel = win.imagepanel
+        proc = panel.processor
+        for paramclass, compute_method, name in (
+            (cdl.param.BlobDOGParam, proc.compute_blob_dog, "BlobDOG"),
+            (cdl.param.BlobDOHParam, proc.compute_blob_doh, "BlobDOH"),
+            (cdl.param.BlobLOGParam, proc.compute_blob_log, "BlobLOG"),
+            (cdl.param.BlobOpenCVParam, proc.compute_blob_opencv, "BlobOpenCV"),
+        ):
+            image = create_image(title, human_mitosis())
+            panel.add_object(image)
+            param = paramclass()
+            title = f"Testing {name} with default parameters"
+            if isinstance(param, cdl.param.BlobOpenCVParam):
+                param.filter_by_color = False
+                title = f"Testing {name} with filter_by_color={param.filter_by_color}"
+            compute_method(param)
+
+
+if __name__ == "__main__":
+    test()
---
+++ cdl/tests/features/image/edges_app.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the terms of the BSD 3-Clause
+# (see cdl/LICENSE for details)
+
+"""
+Edges processing application test
+"""
+
+# pylint: disable=invalid-name  # Allows short reference names like x, y, ...
+
+from skimage.data import human_mitosis  # pylint: disable=no-name-in-module
+
+import cdl.param
+from cdl.core.model.image import create_image
+from cdl.tests import cdl_app_context
+
+SHOW = True  # Show test in GUI-based test launcher
+
+
+def test():
+    """Run ROI unit test scenario"""
+    with cdl_app_context() as win:
+        panel = win.imagepanel
+        proc = panel.processor
+        for paramclass, compute_method, name in (
+            (cdl.param.CannyParam, proc.compute_canny, "Canny filter"),
+            (None, proc.compute_roberts, "Roberts filter"),
+            (None, proc.compute_prewitt, "Prewitt filter"),
+            (None, proc.compute_prewitt_h, "Prewitt horizontal filter"),
+            (None, proc.compute_prewitt_v, "Prewitt vertical filter"),
+            (None, proc.compute_sobel, "Sobel filter"),
+            (None, proc.compute_sobel_h, "Sobel horizontal filter"),
+            (None, proc.compute_sobel_v, "Sobel vertical filter"),
+            (None, proc.compute_scharr, "Scharr filter"),
+            (None, proc.compute_scharr_h, "Scharr horizontal filter"),
+            (None, proc.compute_scharr_v, "Scharr vertical filter"),
+            (None, proc.compute_farid, "Farid filter"),
+            (None, proc.compute_farid_h, "Farid horizontal filter"),
+            (None, proc.compute_farid_v, "Farid vertical filter"),
+            (None, proc.compute_laplace, "Laplace filter"),
+        ):
+            image = create_image(f"Testing {name}", human_mitosis())
+            panel.add_object(image)
+            param = None if paramclass is None else paramclass()
+            if param is None:
+                compute_method()
+            else:
+                compute_method(param)
+
+
+if __name__ == "__main__":
+    test()
