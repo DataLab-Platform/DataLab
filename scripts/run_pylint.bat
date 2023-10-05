9c28964cea2203c8f10df2a882d7d5854d6cb83d
Pierre Raybaut <p.raybaut@codra.fr>
2023-10-05 08:54:53 +0200

    Upgraded run_pylint.bat

--- scripts/run_pylint.bat
+++ scripts/run_pylint.bat
@@ -1,7 +1,7 @@
 @echo off
 REM This script was derived from PythonQwt project
 REM ======================================================
-REM Run pylint code analysis tool
+REM Test launcher script
 REM ======================================================
 REM Licensed under the terms of the MIT License
 REM Copyright (c) 2020 Pierre Raybaut
@@ -9,12 +9,9 @@ REM (see PythonQwt LICENSE file for more details)
 REM ======================================================
 setlocal
 call %~dp0utils GetScriptPath SCRIPTPATH
-call %FUNC% GetLibName LIBNAME
 call %FUNC% GetModName MODNAME
 call %FUNC% SetPythonPath
-call %FUNC% UseWinPython
 set PYLINT_ARG=%*
-if "%PYLINT_ARG%"=="" set PYLINT_ARG=%MODNAME% --disable=fixme
-set RCFILE=%SCRIPTPATH%\..\.pylintrc
-python -m pylint --rcfile=%RCFILE% %PYLINT_ARG%
+if "%PYLINT_ARG%"=="" set PYLINT_ARG=--disable=fixme
+%PYTHON% -m pylint --rcfile=%SCRIPTPATH%\..\.pylintrc %PYLINT_ARG% %MODNAME%
 call %FUNC% EndOfScript
