120066b91e7058770736f6680ff1ffb7ebfcea08
Pierre Raybaut <p.raybaut@codra.fr>
2023-11-13 17:21:31 +0100

    NSIS installer build: fixed .bmp generation

--- scripts/build_installer.bat
+++ scripts/build_installer.bat
@@ -5,22 +5,24 @@ REM ------------------------------------------
 REM Copyright (c) 2021-2023 Codra
 REM ======================================================
 call %~dp0utils GetScriptPath SCRIPTPATH
-set ROOTPATH=%SCRIPTPATH%\..\
+set ROOTPATH=%SCRIPTPATH%\..
+
+set RSCPATH=%ROOTPATH%\resources
 
 @REM Generating images for NSIS installer
 set BMPPATH=%ROOTPATH%\nsis\images
 set INKSCAPE_PATH="C:\Program Files\Inkscape\bin\inkscape.exe"
-%INKSCAPE_PATH% "%BMPPATH%\win.svg" -o "%BMPPATH%\temp.png" -w 164 -h 314
-magick convert "%BMPPATH%\temp.png" "%BMPPATH%\win.bmp"
-%INKSCAPE_PATH% "%BMPPATH%\banner.svg" -o "%BMPPATH%\temp.png" -w 300 -h 114
-magick convert "%BMPPATH%\temp.png" "%BMPPATH%\banner.bmp"
+%INKSCAPE_PATH% "%RSCPATH%\win.svg" -o "%BMPPATH%\temp.png" -w 164 -h 314
+magick convert "%BMPPATH%\temp.png" bmp3:"%BMPPATH%\win.bmp"
+%INKSCAPE_PATH% "%RSCPATH%\banner.svg" -o "%BMPPATH%\temp.png" -w 150 -h 57
+magick convert "%BMPPATH%\temp.png" bmp3:"%BMPPATH%\banner.bmp"
 del "%BMPPATH%\temp.png"
 
 @REM Generating icons for NSIS installer
 set ICOPATH=%ROOTPATH%\nsis\icons
 for %%s in (16 24 32 48 128 256) do (
-  %INKSCAPE_PATH% "%ICOPATH%\install.svg" -o "%ICOPATH%\install-%%s.png" -w %%s -h %%s
-  %INKSCAPE_PATH% "%ICOPATH%\uninstall.svg" -o "%ICOPATH%\uninstall-%%s.png" -w %%s -h %%s
+  %INKSCAPE_PATH% "%RSCPATH%\install.svg" -o "%ICOPATH%\install-%%s.png" -w %%s -h %%s
+  %INKSCAPE_PATH% "%RSCPATH%\uninstall.svg" -o "%ICOPATH%\uninstall-%%s.png" -w %%s -h %%s
 )
 magick convert "%ICOPATH%\install-*.png" "%ICOPATH%\install.ico"
 magick convert "%ICOPATH%\uninstall-*.png" "%ICOPATH%\uninstall.ico"
@@ -34,12 +36,12 @@ call %FUNC% GetVersion VERSION
 call %FUNC% GetVersionWithoutAlphaBeta VERSION_WITHOUT_ALPHABETA
 
 @REM Building NSIS installer
-@REM set NSIS_DIST_PATH=%ROOTPATH%\dist\%LIBNAME%
-@REM set NSIS_PRODUCT_NAME=%LIBNAME%
-@REM set NSIS_PRODUCT_ID=%LIBNAME%
-@REM set NSIS_INSTALLDIR=C:\%LIBNAME%
-@REM set NSIS_PRODUCT_VERSION=%VERSION%
-@REM set NSIS_INSTALLER_VERSION=%VERSION_WITHOUT_ALPHABETA%.0
-@REM "C:\Program Files (x86)\NSIS\makensis.exe" nsis\installer.nsi
+set NSIS_DIST_PATH=%ROOTPATH%\dist\%LIBNAME%
+set NSIS_PRODUCT_NAME=%LIBNAME%
+set NSIS_PRODUCT_ID=%LIBNAME%
+set NSIS_INSTALLDIR=C:\%LIBNAME%
+set NSIS_PRODUCT_VERSION=%VERSION%
+set NSIS_INSTALLER_VERSION=%VERSION_WITHOUT_ALPHABETA%.0
+"C:\Program Files (x86)\NSIS\makensis.exe" nsis\installer.nsi
 
 call %FUNC% EndOfScript
