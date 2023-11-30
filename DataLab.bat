1f99625c9dacd851cf13ee6fa2263ccb4e12496a
Pierre Raybaut <p.raybaut@codra.fr>
2023-11-30 20:56:03 +0100

    Added .bat dev launcher (WinPython)

---
+++ DataLab.bat
@@ -0,0 +1,7 @@
+@echo off
+for %%a in ("%CDL_PYTHONEXE%") do set "p_dir=%%~dpa"
+for %%a in (%p_dir:~0,-1%) do set "WINPYDIRBASE=%%~dpa"
+call %WINPYDIRBASE%scripts\env_for_icons.bat %*
+cd/D %~dp0
+set PYTHONPATH=%cd%
+start "" "%WINPYDIR%\pythonw.exe" -m cdl.app %*
