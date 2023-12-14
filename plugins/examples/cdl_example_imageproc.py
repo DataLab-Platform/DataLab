8c7cbc756435dc6b24e0affbceafcddf2f8cc585
Pierre Raybaut <p.raybaut@codra.fr>
2023-12-14 11:13:23 +0100

    Extract blobs plugin example: added test image generation

--- plugins/examples/cdl_example_imageproc.py
+++ plugins/examples/cdl_example_imageproc.py
@@ -16,6 +16,10 @@ This sub-menu contains two actions, "Preprocess image" and "Detect blobs".
     <https://cdlapp.readthedocs.io/en/latest/features/general/plugins.html>`_).
 """
 
+import numpy as np
+import skimage.draw
+
+import cdl.obj
 import cdl.param
 import cdl.plugins
 
@@ -29,6 +33,24 @@ class ExtractBlobs(cdl.plugins.PluginBase):
         description="This is an example plugin",
     )
 
+    def generate_test_image(self) -> None:
+        """Generate test image"""
+        # Create a NumPy array:
+        arr = np.random.normal(10000, 1000, (2048, 2048))
+        for _ in range(10):
+            row = np.random.randint(0, arr.shape[0])
+            col = np.random.randint(0, arr.shape[1])
+            rr, cc = skimage.draw.disk((row, col), 40, shape=arr.shape)
+            arr[rr, cc] -= np.random.randint(5000, 6000)
+        icenter = arr.shape[0] // 2
+        rr, cc = skimage.draw.disk((icenter, icenter), 200, shape=arr.shape)
+        arr[rr, cc] -= np.random.randint(5000, 8000)
+        data = np.clip(arr, 0, 65535).astype(np.uint16)
+
+        # Create a new image object and add it to the image panel
+        image = cdl.obj.create_image("Test image", data, units=("mm", "mm", "lsb"))
+        self.proxy.add_object(image)
+
     def preprocess(self) -> None:
         """Preprocess image"""
         panel = self.imagepanel
@@ -40,6 +62,9 @@ class ExtractBlobs(cdl.plugins.PluginBase):
         """Detect circular blobs"""
         panel = self.imagepanel
         param = cdl.param.BlobOpenCVParam()
+        param.filter_by_color = False
+        param.min_area = 600.0
+        param.max_area = 6000.0
         param.filter_by_circularity = True
         param.min_circularity = 0.8
         param.max_circularity = 1.0
@@ -49,5 +74,10 @@ class ExtractBlobs(cdl.plugins.PluginBase):
         """Create actions"""
         acth = self.imagepanel.acthandler
         with acth.new_menu(self.PLUGIN_INFO.name):
+            acth.new_action(
+                "Generate test image",
+                triggered=self.generate_test_image,
+                select_condition="always",
+            )
             acth.new_action("Preprocess image", triggered=self.preprocess)
             acth.new_action("Detect circular blobs", triggered=self.detect_blobs)
