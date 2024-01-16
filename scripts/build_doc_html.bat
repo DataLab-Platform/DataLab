0e1e5138cb079cf77fdc7507cfc6dbe29044816c
Pierre Raybaut <p.raybaut@codra.fr>
2024-01-16 10:57:00 +0100

    Quick HTML preview script: removed unnecessary steps

--- scripts/build_doc_html.bat
+++ scripts/build_doc_html.bat
@@ -8,18 +8,9 @@ REM Copyright (c) 2020 Pierre Raybaut
 REM (see PythonQwt LICENSE file for more details)
 REM ======================================================
 call %~dp0utils GetScriptPath SCRIPTPATH
-call %FUNC% GetLibName LIBNAME
-call %FUNC% GetModName MODNAME
 call %FUNC% SetPythonPath
 call %FUNC% UsePython
-call %FUNC% GetVersion CDL_VERSION
 cd %SCRIPTPATH%\..
-%PYTHON% doc\update_requirements.py
-
-@REM Set light mode for Qt applications and clean previous documentation ===============
-set QT_COLOR_MODE=light
-if exist %MODNAME%\data\doc ( rmdir /s /q %MODNAME%\data\doc )
-mkdir %MODNAME%\data\doc
 
 @REM Build documentation ===============================================================
 set LANG=fr
