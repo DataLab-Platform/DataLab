5bce54f1fada78eb65b330bf93aee3c16d13bd21
Pierre Raybaut <p.raybaut@codra.fr>
2024-01-22 15:05:51 +0100

    Update h5_app.py to avoid loading all files at once

--- cdl/tests/features/hdf5/h5_app.py
+++ cdl/tests/features/hdf5/h5_app.py
@@ -22,11 +22,11 @@ def test_h5_app(pattern=None):
     if pattern is None:
         pattern = "*.h5"
     execenv.print("HDF5 import test scenario:")
-    execenv.print("[1] Loading all h5 files at once")
+    execenv.print("[1] Loading all h5 files at once (only the first 5 files)")
     with cdl_app_context(exec_loop=True):
-        app.create(h5files=get_test_fnames(pattern))
-    execenv.print("[2] Loading h5 files one by one (only the first 3 files)")
-    for fname in get_test_fnames(pattern)[:3]:
+        app.create(h5files=get_test_fnames(pattern)[:5])
+    execenv.print("[2] Loading h5 files one by one")
+    for fname in get_test_fnames(pattern):
         with cdl_app_context(exec_loop=True):
             execenv.print(f"      Opening: {fname}")
             app.create(h5files=[fname])
