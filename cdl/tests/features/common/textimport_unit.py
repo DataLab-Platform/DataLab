1a011e41d5c4cce58f92e31e79645c7132a52f21
Pierre Raybaut <p.raybaut@codra.fr>
2024-03-04 15:46:22 +0100

    Text import wizard: added real unit test

--- cdl/tests/features/common/textimport_unit.py
+++ cdl/tests/features/common/textimport_unit.py
@@ -13,6 +13,7 @@ from guidata.qthelpers import exec_dialog, qt_app_context
 from qtpy import QtWidgets as QW
 
 from cdl.env import execenv
+from cdl.obj import ImageObj, SignalObj
 from cdl.tests.data import get_test_fnames
 from cdl.widgets.textimport import TextImportWizard
 
@@ -27,20 +28,40 @@ def file_to_clipboard(filename: str) -> None:
 def test_import_wizard():
     """Test the import wizard"""
     with qt_app_context():
-        for destination, fname in (
-            ("image", "fiber.txt"),
-            ("signal", "paracetamol.txt"),
+        for destination, fname, otype in (
+            ("image", "fiber.txt", ImageObj),
+            ("signal", "paracetamol.txt", SignalObj),
         ):
+            path = get_test_fnames(fname)[0]
             if not execenv.unattended:
                 # Do not test clipboard in unattended mode, would fail:
                 # - Windows: OleSetClipboard: Failed to set mime data (text/plain)
                 #            on clipboard: COM error 0xffffffff800401d0
                 # - Linux:  QXcbClipboard: Unable to receive an event from the clipboard
                 #           manager in a reasonable time
-                path = get_test_fnames(fname)[0]
                 file_to_clipboard(path)
             wizard = TextImportWizard(destination=destination)
-            if exec_dialog(wizard):
+            if execenv.unattended:
+                wizard.show()
+                srcpge = wizard.source_page
+                srcpge.source_widget.file_edit.setText(path)
+                srcpge.source_widget.file_edit.editingFinished.emit()
+                wizard.go_to_next_page()
+                datapge = wizard.data_page
+                if fname == "fiber.txt":
+                    datapge.param.delimiter_choice = " "
+                else:
+                    datapge.param.skip_rows = 10
+                datapge.param_widget.get()
+                datapge.update_preview()
+                wizard.go_to_next_page()
+                wizard.go_to_previous_page()  # For test purpose only
+                wizard.go_to_next_page()
+                wizard.go_to_next_page()
+                wizard.accept()
+                assert len(wizard.get_objs()) == 1
+                assert isinstance(wizard.get_objs()[0], otype)
+            elif exec_dialog(wizard):
                 for obj in wizard.get_objs():
                     execenv.print(obj)
 
