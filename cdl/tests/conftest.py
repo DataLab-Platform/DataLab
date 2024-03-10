e3b9b4d5f566dfc0ed97e525b8cca11d8cbf099b
Pierre Raybaut <pierre.raybaut@gmail.com>
2024-03-10 11:40:38 +0100

    Tests: add fixture to reset current working directory after each test

--- cdl/tests/conftest.py
+++ cdl/tests/conftest.py
@@ -6,8 +6,21 @@ This file contains the configuration for running pytest in DataLab. It is
 executed before running any tests.
 """
 
+import os
+
+import pytest
+
 from cdl.env import execenv
 
 # Turn on unattended mode for executing tests without user interaction
 execenv.unattended = True
 execenv.verbose = "quiet"
+
+INITIAL_CWD = os.getcwd()
+
+
+@pytest.fixture(autouse=True)
+def reset_cwd(request):
+    """Reset the current working directory to the initial one after each test."""
+    yield
+    os.chdir(INITIAL_CWD)
