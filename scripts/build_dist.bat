9dbb172f22e79cc3cd350269747fd2a10cda74f6
Pierre Raybaut <p.raybaut@codra.fr>
2024-04-09 16:50:04 +0200

    Now building source/wheel packages from a clone repo

--- scripts/build_dist.bat
+++ scripts/build_dist.bat
@@ -12,7 +12,28 @@ call %FUNC% GetLibName LIBNAME
 call %FUNC% GetModName MODNAME
 call %FUNC% SetPythonPath
 call %FUNC% UsePython
-if exist MANIFEST ( del /q MANIFEST )
+call %FUNC% GetVersion CDL_VERSION
+
+set REPODIR=%SCRIPTPATH%\..
+
+@REM Clone repository in a temporary directory
+set CLONEDIR=%REPODIR%\..\%LIBNAME%-tempdir
+if exist %CLONEDIR% ( rmdir /s /q %CLONEDIR% )
+git clone -l -s . %CLONEDIR%
+
+pushd %CLONEDIR%
 %PYTHON% create_dephash.py
-%PYTHON% -m build
+@REM Build source package
+%PYTHON% -m build --sdist
+@REM Build wheel package with PDF documentation embedded
+mkdir %MODNAME%\data\doc
+copy %REPODIR%\%MODNAME%\data\doc\*.pdf %MODNAME%\data\doc
+%PYTHON% -m build --wheel
+popd
+
+if not exist %REPODIR%\dist ( mkdir %REPODIR%\dist )
+copy %CLONEDIR%\dist\%MODNAME%-%CDL_VERSION%*.whl %REPODIR%\dist
+copy %CLONEDIR%\dist\%MODNAME%-%CDL_VERSION%*.tar.gz %REPODIR%\dist
+
+rmdir /s /q %CLONEDIR%
 call %FUNC% EndOfScript
