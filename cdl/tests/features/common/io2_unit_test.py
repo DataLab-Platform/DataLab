663328224775a68b72a107c60e6049baf3ae4789
Pierre Raybaut <p.raybaut@codra.fr>
2024-04-11 16:26:12 +0200

    io2_unit_test.py: remove Qt dependency from test

--- cdl/tests/features/common/io2_unit_test.py
+++ cdl/tests/features/common/io2_unit_test.py
@@ -12,14 +12,11 @@ from __future__ import annotations
 
 import os.path as osp
 
-from guidata.qthelpers import qt_app_context
-
 from cdl.core.io.base import BaseIORegistry
 from cdl.core.io.image import ImageIORegistry
 from cdl.core.io.signal import SignalIORegistry
 from cdl.env import execenv
 from cdl.plugins import discover_plugins
-from cdl.utils.qthelpers import CallbackWorker, qt_long_callback
 from cdl.utils.strings import reduce_path
 from cdl.utils.tests import CDLTemporaryDirectory, get_test_fnames
 
@@ -36,15 +33,11 @@ def __testfunc(
         fnames = get_test_fnames(pattern, in_folder)
         objects = {}
         for fname in fnames:
-            worker = CallbackWorker(
-                lambda worker, fname: registry.read(fname, worker)[0], fname=fname
-            )
             label = f"    Opening {reduce_path(fname)}"
             execenv.print(label + ": ", end="")
-            worker.SIG_PROGRESS_UPDATE.connect(lambda value: execenv.print(">", end=""))
             try:
-                obj = qt_long_callback(None, label, worker, fname.endswith(".sif"))
-                execenv.print("Canceled" if worker.was_canceled() else "OK")
+                obj = registry.read(fname)[0]
+                execenv.print("OK")
                 objects[fname] = obj
             except NotImplementedError:
                 execenv.print("Skipped (not implemented)")
@@ -65,9 +58,8 @@ def __testfunc(
 def test_io2():
     """I/O test"""
     execenv.print("I/O unit test:")
-    with qt_app_context():
-        __testfunc("Signals", SignalIORegistry, "*.*", "curve_formats")
-        __testfunc("Images", ImageIORegistry, "*.*", "image_formats")
+    __testfunc("Signals", SignalIORegistry, "*.*", "curve_formats")
+    __testfunc("Images", ImageIORegistry, "*.*", "image_formats")
 
 
 if __name__ == "__main__":
