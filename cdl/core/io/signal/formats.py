3750cf2b99464dc000fa0b5d01ab06afa916c895
vld-codra <v.ledu@codra.fr>
2024-04-19 16:41:07 +0200

    first implementation of .mat r/w

--- cdl/core/io/signal/formats.py
+++ cdl/core/io/signal/formats.py
@@ -6,15 +6,18 @@ DataLab I/O signal formats
 
 from __future__ import annotations
 
+import os
 from typing import TYPE_CHECKING
 
 import numpy as np
+import scipy.io as sio
 
 from cdl.config import _
 from cdl.core.io.base import FormatInfo
 from cdl.core.io.conv import convert_array_to_standard_type
 from cdl.core.io.signal import funcs
 from cdl.core.io.signal.base import SignalFormatBase
+from cdl.core.model.signal import SignalObj
 
 if TYPE_CHECKING:
     from cdl.core.model.signal import SignalObj
@@ -108,3 +111,51 @@ class NumPySignalFormat(SignalFormatBase):
             obj: Signal object to read data from
         """
         np.save(filename, obj.xydata.T)
+
+
+class MatSignalFormat(SignalFormatBase):
+    """Object representing a Matlab .mat signal file type"""
+
+    FORMAT_INFO = FormatInfo(
+        name=_("Matlab .mat files"),
+        extensions="*.mat",
+        readable=True,
+        writeable=True,
+    )  # pylint: disable=duplicate-code
+
+    def read(self, filename: str, worker: CallbackWorker) -> list[SignalObj]:
+        """Read data and metadata from file, write metadata to object, return xydata
+
+        Args:
+            filename: Name of file to read
+
+        Returns:
+            NumPy array xydata
+        """
+        mat = sio.loadmat(filename)
+        metadata = {k: v for k, v in mat.items() if k.startswith("__")}
+        datas = {
+            k: v
+            for k, v in mat.items()
+            if not k.startswith("__") and isinstance(v, np.ndarray)
+        }
+
+        signals: list[SignalObj] = []
+        for k, data in datas.items():
+            data = data.squeeze()
+            signals.extend(self.create_signals_from(data, filename))
+        for signal, dname in zip(signals, datas.keys()):
+            signal.title += f" ({dname})"  # type: ignore
+            signal.metadata.update(metadata)
+        return signals
+
+    def write(self, filename: str, obj: SignalObj) -> None:
+        """Write data to file
+
+        Args:
+            filename: Name of file to write
+            obj: Signal object to read data from
+        """
+        # metadata cannot be saved as such as their type will be lost and
+        # cause problems when reading the file back
+        sio.savemat(filename, {obj.get_title(): obj.xydata.T})
