bfd3773312300dd972d7d2e0e28c3d3c2078e7c6
Pierre Raybaut <pierre.raybaut@gmail.com>
2024-05-22 21:15:35 +0200

    Add icon for importing text file

--- cdl/core/gui/actionhandler.py
+++ cdl/core/gui/actionhandler.py
@@ -408,6 +408,7 @@ class BaseActionHandler(metaclass=abc.ABCMeta):
             )
             self.new_action(
                 _("Import text file..."),
+                icon=get_icon("import.svg"),
                 triggered=self.panel.exec_import_wizard,
                 select_condition=SelectCond.always,
             )
