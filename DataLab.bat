1afe1e86adef2596635cf39cc1a665764cad121d
Pierre Raybaut <p.raybaut@codra.fr>
2024-08-27 15:05:20 +0200

    Update `DataLab.bat` to use `cdl\start.pyw` instead of `cdl.app` for launching the application

--- DataLab.bat
+++ DataLab.bat
@@ -4,4 +4,4 @@ for %%a in (%p_dir:~0,-1%) do set "WINPYDIRBASE=%%~dpa"
 call %WINPYDIRBASE%scripts\env_for_icons.bat %*
 cd/D %~dp0
 set PYTHONPATH=%cd%
-start "" "%WINPYDIR%\pythonw.exe" -m cdl.app %*
+start "" "%WINPYDIR%\pythonw.exe" cdl\start.pyw %*
