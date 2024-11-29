Fix amplitude calculation in create_2d_random for non-integer dtypes

cdl/tests/data.py
@@ -17,6 +17,7 @@ import guidata.dataset as gds
 import numpy as np
 
 import cdl.obj
+from cdl.algorithms.datatypes import is_integer_dtype
 from cdl.config import _
 from cdl.utils.tests import get_test_fnames
 
@@ -215,7 +216,7 @@ def create_2d_random(
         2D data
     """
     rng = np.random.default_rng(seed)
-    amp = np.iinfo(dtype).max * level
+    amp = (np.iinfo(dtype).max if is_integer_dtype(dtype) else 1.0) * level
     return np.array(rng.random((size, size)) * amp, dtype=dtype)
 
 