Fix: ignore NaN values in signal normalization and statistics computations Fix #153
(cherry picked from commit c153fab9d2cfb7aa17873237565d254f9b566996)

cdl/algorithms/signal.py
@@ -36,24 +36,24 @@ def normalize(
     """
     axis = len(yin.shape) - 1
     if parameter == "maximum":
-        maximum = np.max(yin, axis)
+        maximum = np.nanmax(yin, axis)
         if axis == 1:
             maximum = maximum.reshape((len(maximum), 1))
         maxarray = np.tile(maximum, yin.shape[axis]).reshape(yin.shape)
         return yin / maxarray
     if parameter == "amplitude":
         ytemp = np.array(yin, copy=True)
-        minimum = np.min(yin, axis)
+        minimum = np.nanmin(yin, axis)
         if axis == 1:
             minimum = minimum.reshape((len(minimum), 1))
         ytemp -= minimum
         return normalize(ytemp, parameter="maximum")
     if parameter == "area":
-        return yin / yin.sum()
+        return yin / np.nansum(yin)
     if parameter == "energy":
-        return yin / np.sqrt(np.sum(yin * yin.conjugate()))
+        return yin / np.sqrt(np.nansum(yin * yin.conjugate()))
     if parameter == "rms":
-        return yin / np.sqrt(np.mean(yin * yin.conjugate()))
+        return yin / np.sqrt(np.nanmean(yin * yin.conjugate()))
     raise RuntimeError(f"Unsupported parameter {parameter}")
 
 

cdl/computation/signal.py
@@ -1611,14 +1611,14 @@ def compute_stats(obj: SignalObj) -> ResultProperties:
         Result properties object
     """
     statfuncs = {
-        "min(y) = %g {.yunit}": lambda xy: xy[1].min(),
-        "max(y) = %g {.yunit}": lambda xy: xy[1].max(),
-        "<y> = %g {.yunit}": lambda xy: xy[1].mean(),
-        "median(y) = %g {.yunit}": lambda xy: np.median(xy[1]),
-        "σ(y) = %g {.yunit}": lambda xy: xy[1].std(),
-        "<y>/σ(y)": lambda xy: xy[1].mean() / xy[1].std(),
-        "peak-to-peak(y) = %g {.yunit}": lambda xy: np.ptp(xy[1]),
-        "Σ(y) = %g {.yunit}": lambda xy: xy[1].sum(),
+        "min(y) = %g {.yunit}": lambda xy: np.nanmin(xy[1]),
+        "max(y) = %g {.yunit}": lambda xy: np.nanmax(xy[1]),
+        "<y> = %g {.yunit}": lambda xy: np.nanmean(xy[1]),
+        "median(y) = %g {.yunit}": lambda xy: np.nanmedian(xy[1]),
+        "σ(y) = %g {.yunit}": lambda xy: np.nanstd(xy[1]),
+        "<y>/σ(y)": lambda xy: np.nanmean(xy[1]) / np.nanstd(xy[1]),
+        "peak-to-peak(y) = %g {.yunit}": lambda xy: np.nanmax(xy[1]) - np.nanmin(xy[1]),
+        "Σ(y) = %g {.yunit}": lambda xy: np.nansum(xy[1]),
         "∫ydx": lambda xy: spt.trapezoid(xy[1], xy[0]),
     }
     return calc_resultproperties("stats", obj, statfuncs)