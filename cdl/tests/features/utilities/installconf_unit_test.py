269de0614e9155127b998b798ad080092f3bee84
Pierre Raybaut <p.raybaut@codra.fr>
2025-05-14 15:49:03 +0200

    Refactor installation and configuration viewer documentation and tests for clarity and completeness

--- cdl/tests/features/utilities/installconf_unit_test.py
+++ cdl/tests/features/utilities/installconf_unit_test.py
@@ -6,16 +6,23 @@ Dependencies viewer test
 
 # guitest: show
 
+from __future__ import annotations
+
 from guidata.qthelpers import qt_app_context
 
-from cdl.widgets.instconfviewer import exec_cdl_installconfig_dialog
+import cdl.utils.qthelpers as qth
+from cdl.widgets import instconfviewer
 
 
-def test_dep_viewer():
+def test_dep_viewer(screenshots: bool = False) -> None:
     """Test dep viewer window"""
     with qt_app_context():
-        exec_cdl_installconfig_dialog()
+        instconfviewer.exec_cdl_installconfig_dialog()
+        if screenshots:
+            dlg = instconfviewer.InstallConfigViewerWindow()
+            dlg.show()
+            qth.grab_save_window(dlg, dlg.objectName())
 
 
 if __name__ == "__main__":
-    test_dep_viewer()
+    test_dep_viewer(screenshots=True)
