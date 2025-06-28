e11a4848b3663e15778c59b58163d2651f44b492
Pierre Raybaut <p.raybaut@codra.fr>
2025-06-28 10:47:05 +0200

    Enhance import wizard tests: add parameter validation and update dataset checks for image and signal objects

--- cdl/tests/features/common/textimport_unit_test.py
+++ cdl/tests/features/common/textimport_unit_test.py
@@ -6,13 +6,15 @@ Import wizard test
 
 # guitest: show
 
+import numpy as np
+from guidata.dataset import update_dataset
 from guidata.qthelpers import exec_dialog, qt_app_context
 from qtpy import QtWidgets as QW
 
 from cdl.env import execenv
 from cdl.obj import ImageObj, SignalObj
 from cdl.tests.data import get_test_fnames
-from cdl.widgets.textimport import TextImportWizard
+from cdl.widgets.textimport import ImageParam, SignalParam, TextImportWizard
 
 
 def file_to_clipboard(filename: str) -> None:
@@ -25,6 +27,23 @@ def file_to_clipboard(filename: str) -> None:
             return
         except UnicodeDecodeError:
             pass
+
+
+TEST_SIGNAL_PARAM = SignalParam.create(
+    xlabel="X Signal axis",
+    ylabel="Y Signal axis",
+    xunit="X Signal unit",
+    yunit="Y Signal unit",
+)
+
+TEST_IMAGE_PARAM = ImageParam.create(
+    xlabel="X Image axis",
+    ylabel="Y Image axis",
+    zlabel="Z Image axis",
+    xunit="X Image unit",
+    yunit="Y Image unit",
+    zunit="Z Image unit",
+)
 
 
 def test_import_wizard():
@@ -46,12 +65,16 @@ def test_import_wizard():
             wizard = TextImportWizard(destination=destination)
             if execenv.unattended:
                 wizard.show()
-                srcpge = wizard.source_page
+                srcpge = wizard.source_page  # `SourcePage`
                 srcpge.param.path = path
                 srcpge.param_widget.get()
                 wizard.go_to_next_page()
-                datapge = wizard.data_page
+                datapge = wizard.data_page  # `DataPreviewPage`
                 n_objs = 1
+                if destination == "image":
+                    datapge.param.dtype_str = "uint8"
+                else:
+                    datapge.param.dtype_str = "float32"
                 if fname == "fiber.txt":
                     datapge.param.delimiter_choice = " "
                 elif fname == "multiple_curves.csv":
@@ -62,13 +85,37 @@ def test_import_wizard():
                     datapge.param.skip_rows = 10
                 datapge.param_widget.get()
                 datapge.update_preview()
-                wizard.go_to_next_page()
+                wizard.go_to_next_page()  # Go to `GraphicalRepresentationPage`
                 wizard.go_to_previous_page()  # For test purpose only
-                wizard.go_to_next_page()
-                wizard.go_to_next_page()
+                wizard.go_to_next_page()  # Go to `GraphicalRepresentationPage`
+                labels_page = wizard.labels_page  # `LabelsPage`
+                if destination == "image":
+                    assert isinstance(labels_page.param, ImageParam)
+                    update_dataset(labels_page.param, TEST_IMAGE_PARAM)
+                else:
+                    assert isinstance(labels_page.param, SignalParam)
+                    update_dataset(labels_page.param, TEST_SIGNAL_PARAM)
+                wizard.go_to_next_page()  # Go to `LabelsPage`
                 wizard.accept()
-                assert len(wizard.get_objs()) == n_objs
-                assert isinstance(wizard.get_objs()[0], otype)
+                objs = wizard.get_objs()
+                assert len(objs) == n_objs
+                assert isinstance(objs[0], otype)
+                # Check that the parameters are set correctly
+                for obj in objs:
+                    if isinstance(obj, ImageObj):
+                        assert obj.data.dtype == np.uint8
+                        assert obj.xlabel == TEST_IMAGE_PARAM.xlabel
+                        assert obj.ylabel == TEST_IMAGE_PARAM.ylabel
+                        assert obj.zlabel == TEST_IMAGE_PARAM.zlabel
+                        assert obj.xunit == TEST_IMAGE_PARAM.xunit
+                        assert obj.yunit == TEST_IMAGE_PARAM.yunit
+                        assert obj.zunit == TEST_IMAGE_PARAM.zunit
+                    elif isinstance(obj, SignalObj):
+                        assert obj.data.dtype == np.float32
+                        assert obj.xlabel == TEST_SIGNAL_PARAM.xlabel
+                        assert obj.ylabel == TEST_SIGNAL_PARAM.ylabel
+                        assert obj.xunit == TEST_SIGNAL_PARAM.xunit
+                        assert obj.yunit == TEST_SIGNAL_PARAM.yunit
             elif exec_dialog(wizard):
                 for obj in wizard.get_objs():
                     execenv.print(obj)
