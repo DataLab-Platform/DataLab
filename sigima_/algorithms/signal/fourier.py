fix bug #241 on ifft1d computation

sigima_/algorithms/signal/fourier.py
@@ -69,7 +69,8 @@ def ifft1d(
         y = np.fft.ifftshift(y)
     y1 = np.fft.ifft(y)
     # Recalculate the original time domain array
-    dt = 1.0 / (x[-1] - x[0] + (x[1] - x[0]))
+    x_sorted = np.sort(x)
+    dt = 1.0 / (x_sorted[-1] - x_sorted[0] + (x_sorted[1] - x_sorted[0]))
     x1 = np.arange(y1.size) * dt
     return x1, y1.real
 