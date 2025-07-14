87e654c1125ab2eaf396afacaa4f2a6756935c40
Pierre Raybaut <p.raybaut@codra.fr>
2025-07-14 11:58:06 +0200

    Refactor test_dict_serialization to use create_test_image_with_metadata from Sigima

--- datalab/tests/backbone/dictlistserial_app_test.py
+++ datalab/tests/backbone/dictlistserial_app_test.py
@@ -8,9 +8,7 @@ Testing (de)serialization of Dictionnary/List inside object metadata
 
 import os.path as osp
 
-import numpy as np
-from sigima.objects import create_image
-from sigima.tests.data import get_test_image
+from sigima.tests.data import create_test_image_with_metadata
 from sigima.tests.helpers import WorkdirRestoringTempDir, compare_metadata
 
 from datalab.env import execenv
@@ -23,39 +21,7 @@ def test_dict_serialization():
         with WorkdirRestoringTempDir() as tmpdir:
             with cdltest_app_context(console=False) as win:
                 panel = win.imagepanel
-
-                data = get_test_image("flower.npy").data
-                image = create_image("Test image with peaks", data)
-                image.metadata["tata"] = {
-                    "lkl": 2,
-                    "tototo": 3,
-                    "arrdata": np.array([0, 1, 2, 3, 4, 5]),
-                    "zzzz": "lklk",
-                    "bool": True,
-                    "float": 1.234,
-                    "list": [1, 2.5, 3, "str", False, 5],
-                    "d": {
-                        "lkl": 2,
-                        "tototo": 3,
-                        "zzzz": "lklk",
-                        "bool": True,
-                        "float": 1.234,
-                        "list": [
-                            1,
-                            2.5,
-                            3,
-                            "str",
-                            False,
-                            5,
-                            {"lkl": 2, "l": [1, 2, 3]},
-                        ],
-                    },
-                }
-                image.metadata["toto"] = [
-                    np.array([[1, 2], [-3, 0]]),
-                    np.array([[1, 2], [-3, 0], [99, 241]]),
-                ]
-                image.metadata["array"] = np.array([-5, -4, -3, -2, -1])
+                image = create_test_image_with_metadata()
                 panel.add_object(image)
                 fname = osp.join(tmpdir, "test.h5")
                 win.save_to_h5_file(fname)
