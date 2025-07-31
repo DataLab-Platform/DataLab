DataLab-Platform/DataLab

commit 1d7e2675b8c84e163068e8e49d7c1c43bc88382f

Add "Show console on error" option in settings and update related functionality

datalab/gui/main.py
@@ -1069,6 +1069,19 @@ class DLMainWindow(QW.QMainWindow, AbstractDLControl, metaclass=DLMainWindowMeta
         ]
         add_actions(self.help_menu, help_menu_actions)
 
+    def __update_console_show_mode(self) -> None:
+        """Update console show mode from configuration option
+
+        Console show mode is whether the console is shown or not when an error occurs.
+        """
+        if self.console is not None:
+            state = Conf.console.show_console_on_error.get()
+            self.console.setVisible(state)
+            if state:
+                self.console.exception_occurred.connect(self.console.show_console)
+            else:
+                self.console.exception_occurred.disconnect(self.console.show_console)
+
     def __setup_console(self) -> None:
         """Add an internal console"""
         ns = {
@@ -1102,6 +1115,7 @@ class DLMainWindow(QW.QMainWindow, AbstractDLControl, metaclass=DLMainWindowMeta
         self.console.interpreter.widget_proxy.sig_new_prompt.connect(
             lambda txt: self.repopulate_panel_trees()
         )
+        self.__update_console_show_mode()
 
     def __add_macro_panel(self) -> None:
         """Add macro panel"""
@@ -1690,6 +1704,7 @@ class DLMainWindow(QW.QMainWindow, AbstractDLControl, metaclass=DLMainWindowMeta
         changed_options = edit_settings(self)
         sigima_options.fft_shift_enabled.set(Conf.proc.fft_shift_enabled.get())
         sigima_options.keep_results.set(Conf.proc.keep_results.get())
+        self.__update_console_show_mode()
         for option in changed_options:
             if option == "color_mode":
                 self.__update_color_mode()

datalab/config.py
@@ -122,6 +122,7 @@ class ConsoleSection(conf.Section, metaclass=conf.SectionMeta):
     option names in .INI file based on class attribute names)."""
 
     console_enabled = conf.Option()
+    show_console_on_error = conf.Option()
     max_line_count = conf.Option()
     external_editor_path = conf.Option()
     external_editor_args = conf.Option()
@@ -309,6 +310,7 @@ def initialize():
     Conf.main.tour_enabled.get(True)
     # Console section
     Conf.console.console_enabled.get(True)
+    Conf.console.show_console_on_error.get(False)
     Conf.console.external_editor_path.get("code")
     Conf.console.external_editor_args.get("-g {path}:{line_number}")
     # IO section

datalab/gui/settings.py
@@ -77,6 +77,15 @@ class ConsoleSettings(gds.DataSet):
         _("Settings for internal console, used for debugging or advanced users")
     )
     console_enabled = gds.BoolItem("", _("Console enabled"))
+    show_console_on_error = gds.BoolItem(
+        "",
+        _("Show console on error"),
+        help=_(
+            "If enabled, the console will be shown when an error occurs in the "
+            "application. This is useful for debugging purposes as it allows to "
+            "see the error traceback."
+        ),
+    )
     external_editor_path = gds.StringItem(_("External editor path"))
     external_editor_args = gds.StringItem(_("External editor arguments"))
     _g0 = gds.EndGroup("")

datalab/locale/fr/LC_MESSAGES/datalab.po
@@ -6,7 +6,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version:  datalab\n"
 "Report-Msgid-Bugs-To: p.raybaut@codra.fr\n"
-"POT-Creation-Date: 2025-07-08 14:46+0200\n"
+"POT-Creation-Date: 2025-07-15 10:28+0200\n"
 "PO-Revision-Date: 2025-05-22 15:46+0200\n"
 "Last-Translator: Christophe Debonnel <c.debonnel@codra.fr>\n"
 "Language: fr\n"
@@ -995,6 +995,9 @@ msgstr "Médiane mobile"
 msgid "Wiener filter"
 msgstr "Filtre de Wiener"
 
+msgid "Gaussian frequency filter"
+msgstr "Filtre fréquentiel gaussien"
+
 msgid "Zero padding"
 msgstr "Complément de zéros"
 
@@ -1005,13 +1008,13 @@ msgid "FFT"
 msgstr "FFT"
 
 msgid "Fast Fourier Transform (FFT) is an estimation of the Discrete Fourier Transform (DFT). Results are complex numbers, but only the real part is plotted."
-msgstr ""
+msgstr "La transformée de Fourier rapide (FFT) est une estimation de la transformée de Fourier discrète (DFT). Les résultats sont des nombres complexes, mais seule la partie réelle est tracée."
 
 msgid "Inverse FFT"
 msgstr "FFT inverse"
 
 msgid "Inverse Fast Fourier Transform (IFFT) is an estimation of the Inverse Discrete Fourier Transform (IDFT). Results are complex numbers, but only the real part is plotted."
-msgstr ""
+msgstr "La transformée de Fourier rapide inverse (IFFT) est une estimation de la transformée de Fourier discrète inverse (IDFT). Les résultats sont des nombres complexes, mais seule la partie réelle est tracée."
 
 msgid "Magnitude spectrum"
 msgstr "Spectre d'amplitude"
@@ -1449,6 +1452,12 @@ msgstr "Réglages de la console interne, utilisée pour le débogage ou les util
 
 msgid "Console enabled"
 msgstr "Console activée"
+
+msgid "Show console on error"
+msgstr "Afficher la console en cas d'erreur"
+
+msgid "If enabled, the console will be shown when an error occurs in the application. This is useful for debugging purposes as it allows to see the error traceback."
+msgstr "Si activé, la console sera affichée lorsqu'une erreur se produit dans l'application. Cela est utile pour le débogage car cela permet de voir la trace de l'erreur."
 
 msgid "External editor path"
 msgstr "Chemin de l'éditeur externe"
@@ -2332,4 +2341,3 @@ msgstr "Merci de sélectionner le fichier à importer."
 
 msgid "Example Wizard"
 msgstr "Assistant exemple"
-

commit fabbbbae0ded399fa382e03cadca8cb894d47335

Fix: embedded console was broken since commit 1d7e267

datalab/gui/main.py
@@ -157,7 +157,7 @@ class DLMainWindow(QW.QMainWindow, AbstractDLControl, metaclass=DLMainWindowMeta
         self.imagepanel: ImagePanel | None = None
         self.tabwidget: QW.QTabWidget | None = None
         self.tabmenu: QW.QMenu | None = None
-        self.docks: dict[AbstractPanel, QW.QDockWidget] | None = None
+        self.docks: dict[AbstractPanel | DockableConsole, QW.QDockWidget] | None = None
         self.h5inputoutput = H5InputOutput(self)
 
         self.openh5_action: QW.QAction | None = None
@@ -933,10 +933,10 @@ class DLMainWindow(QW.QMainWindow, AbstractDLControl, metaclass=DLMainWindowMeta
         self.tabwidget = QW.QTabWidget()
         self.tabmenu = add_corner_menu(self.tabwidget)
         configure_menu_about_to_show(self.tabmenu, self.__update_tab_menu)
-        cdock = self.__add_dockwidget(self.__add_signal_panel(), title=_("Signal View"))
+        sdock = self.__add_dockwidget(self.__add_signal_panel(), title=_("Signal View"))
         idock = self.__add_dockwidget(self.__add_image_panel(), title=_("Image View"))
-        self.tabifyDockWidget(cdock, idock)
-        self.docks = {self.signalpanel: cdock, self.imagepanel: idock}
+        self.tabifyDockWidget(sdock, idock)
+        self.docks = {self.signalpanel: sdock, self.imagepanel: idock}
         self.tabwidget.currentChanged.connect(self.__tab_index_changed)
         self.signalpanel.SIG_OBJECT_ADDED.connect(
             lambda: self.set_current_panel("signal")
@@ -1076,7 +1076,9 @@ class DLMainWindow(QW.QMainWindow, AbstractDLControl, metaclass=DLMainWindowMeta
         """
         if self.console is not None:
             state = Conf.console.show_console_on_error.get()
-            self.console.setVisible(state)
+            cdock = self.docks[self.console]
+            if not state and cdock.isVisible():
+                cdock.hide()
             if state:
                 self.console.exception_occurred.connect(self.console.show_console)
             else:
@@ -1110,8 +1112,9 @@ class DLMainWindow(QW.QMainWindow, AbstractDLControl, metaclass=DLMainWindowMeta
         self.console = DockableConsole(self, namespace=ns, message=msg, debug=DEBUG)
         self.console.setMaximumBlockCount(Conf.console.max_line_count.get(5000))
         self.console.go_to_error.connect(go_to_error)
-        console_dock = self.__add_dockwidget(self.console, _("Console"))
-        console_dock.hide()
+        cdock = self.__add_dockwidget(self.console, _("Console"))
+        self.docks[self.console] = cdock
+        cdock.hide()
         self.console.interpreter.widget_proxy.sig_new_prompt.connect(
             lambda txt: self.repopulate_panel_trees()
         )